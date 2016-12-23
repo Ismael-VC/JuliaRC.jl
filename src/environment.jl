1ddc3de778607395ca7b6fb68d54e2e0f96f90ce
Ismael Venegas Castelló <Ismael-VC@users.noreply.github.com>
2016-12-23 14:37:31 -0600

    Add atom as default editor.

--- src/environment.jl
+++ src/environment.jl
@@ -1,6 +1,7 @@
 ENV["PYTHON"] = ""
 ENV["JULIA_WARN_COLOR"] = :yellow
 ENV["JULIA_INFO_COLOR"] = :cyan
+ENV["EDITOR"] = "C:\\Users\\Ismael\\AppData\\Local\\atom\\app-1.12.7\\atom.exe"
 # ENV["LISP_PROMPT_TEXT"]  = "λ ↦ "
 
 @static if is_windows()
